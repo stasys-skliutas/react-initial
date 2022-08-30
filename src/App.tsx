680802bbf7ac2af5045706d1eae4ad455bbcd7c7
Stas <stas@stass-macbook-air.local>
2022-08-30 20:12:07 +0300

    Init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styled from "styled-components";
+
+const StyledImg = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+`;
+
+const StyledAppContainer = styled.div`
+  text-align: center;
+
+  @media (prefers-reduced-motion: no-preference) {
+    ${StyledImg} {
+      animation: App-logo-spin infinite 20s linear;
+    }
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const StyledHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const StyledLink = styled.a`
+  color: #61dafb;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <StyledAppContainer>
+      <StyledHeader>
+        <StyledImg src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <StyledLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </StyledLink>
+      </StyledHeader>
+    </StyledAppContainer>
   );
 }
 
